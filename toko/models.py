0bb1b10317dfda4c2815d22692675cdc4b6686f7
soedarhanaINJ <i.soedarhana@gmail.com>
2023-08-19 00:04:01 +0900

    Create class & Func model

--- toko/models.py
+++ toko/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# class category for category on product site
+class Category(models.Model):
+    name = models.CharField(max_length=125)
+
+    def __str__(self):
+        return self.name
+    
+# class product
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    harga = models.DecimalField(max_digits=10, decimal_places=3)
+    deskripsi = models.TextField()
+    gambar = models.ImageField(upload_to='media')
+
+    def __str__(self):
+        return self.name
+    
+# class Order
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    produks = models.ManyToManyField(Product, through='OrderItem')
+    total_harga = models.DecimalField(max_digits=10, decimal_places=3)
+    tanggal_order = models.DateTimeField(auto_now_add=True)
+    is_order = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id}"
+    
+# class order item for ordering some item
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    produk = models.ForeignKey(Product, on_delete=models.CASCADE)
+    jumlah = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.produk.name} - {self.jumlah}"
+    
--- toko/views.py
+++ toko/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Category, Product, Order, OrderItem
 
-class index(request):
-    return render(request, 'index.html', )
+def produk_list(request):
+    produks = Product.objects.all()
+
+    return render(request, 'produklist.html', {'produks': produks})
+
+def produk_detail(request):
+    produk = get_object_or_404(Product, pk=produk_id)
+
+    return render(request, 'template/produkdetail.html', {'produk': produk})
+
+# login account need for add item to chart
+@login_required
+def tambah_keranjang(request):
+    produk = get_object_or_404(Product, pk=produk_id)
+    order, created = Order.objects.get_or_create(user=request.user, is_ordered=False)
+    order_item, created = OrderItem.objects.get_or_create(order=order, produk=produk)
+    order_item.jumlah += 1
+    order_item.save()
+
+    return redirect('produk_list')
+
+# login account need for view the chart
+@login_required
+def lihat_keranjang(request):
+    order = Order.objects.get(user=request.user, is_order=False)
+    order_items = order.orderitem_set.all()
+    
+    return render(request, 'template/lihatkeranjang.html', {'order': order, 'order_items': order_items})
+
+# login account need for checkout the item
+@login_required
+def checkout(request):
+    order = Order.objects.get(user=request.user, is_order=False)
+    order.is_ordered = True
+    order.save()
+
+    return render(request, 'template/checkout.html', {'order': order})
+
