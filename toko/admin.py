1a205df60ef0aa00b62b10f601ea687e3675fe5b
soedarhanaINJ <i.soedarhana@gmail.com>
2023-08-21 22:34:50 +0900

    Add media

--- toko/admin.py
+++ toko/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Category, Product
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Category, AdminCategory)
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('category', 'name', 'deskripsi')
